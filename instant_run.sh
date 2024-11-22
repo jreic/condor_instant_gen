e3190da88a92cfbccc10d5c4f8d7e9b7c2159eca
Joseph Reichert <joseph.reichert@cern.ch>
2024-11-22 18:23:15 -0500

    ext8

--- instant_run.sh
+++ instant_run.sh
@@ -52,7 +52,7 @@ cmsswAodDir=$genToRecoBase/CMSSW_10_6_30_patch1/src
 cmsswMiniAodDir=$genToRecoBase/CMSSW_10_6_30_patch1/src
 
 nevents=2500
-randomseed=$((365*$year+$2))
+randomseed=$((444*$year+$2))
 
 
 echo -e "\nStart Sherpa\n"
