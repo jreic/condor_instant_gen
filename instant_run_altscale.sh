df05f9fda9aec272716900dc8fd2a1ee4073d74b
Joseph Reichert <joseph.reichert@cern.ch>
2025-03-12 09:03:53 -0400

    altscale ext2

--- instant_run_altscale.sh
+++ instant_run_altscale.sh
@@ -52,7 +52,7 @@ cmsswAodDir=$genToRecoBase/CMSSW_10_6_30_patch1/src
 cmsswMiniAodDir=$genToRecoBase/CMSSW_10_6_30_patch1/src
 
 nevents=2500
-randomseed=$((732*$year+$2))
+randomseed=$((750*$year+$2))
 
 
 echo -e "\nStart Sherpa\n"
